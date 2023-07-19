1451f966fdc9e856c4f96ce625ea49d09d08de2a
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-07-19 19:04:43 -0400

    user commenting

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
 --  script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
-SHOW GRANTS FOR user_0d_1@localhost;
-
--- lists all granted privileged for two user in a database
-
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost, user_0d_2@localhost;
 
