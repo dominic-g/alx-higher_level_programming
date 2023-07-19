088b94454bf824dad10e02e97c84054008c6a71d
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-07-19 16:53:36 -0400

    create user commenting

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
-FLUSH PRIVILEGES;
+-- create a new user
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- this is to grant privileges
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
