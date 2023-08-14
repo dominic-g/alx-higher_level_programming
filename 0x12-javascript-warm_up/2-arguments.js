c075c920022989eb11bf8c4286891546681aa250
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-14 18:34:24 -0400

    edited: 2-arguments.js 8 file(s)  created:  Pushed at 2023-08-14 18:...

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 //A script that prints a message depending of the number of arguments passed
 
-if (process.argv.length === 2) {
+if (process.argv.length == 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (process.argv.length == 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
