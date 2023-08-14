d72c2384087c7c9e2d1216e8af3bff1f37e597da
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-14 18:30:51 -0400

    edited: 10-factorial.js 8 file(s)  created:  Pushed at 2023-08-14 18...

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 // a script that computes and prints a factorial
 
-function factorial (x) {
-  if ((isNaN(x)) || (x === 1)) {
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
     return 1;
   } else {
-      return x * factorial(x - 1);
-    }
+    return n * factorial(n - 1);
+  }
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+let firstArg = process.argv[2];
+firstArg = parseInt(firstArg);
+console.log(factorial(firstArg));
