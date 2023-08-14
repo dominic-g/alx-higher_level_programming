cdb3641598bb3524c17ed57e3c4b42d0e585faaa
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-14 19:00:56 -0400

    Removing comments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-//A script that prints a message depending of the number of arguments passed
-
 const totalArguments = process.argv.length - 2;
 if (totalArguments === 0) {
   console.log('No argument');
