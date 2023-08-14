e1417caf15f4814b9fabe3b386cfe20bcdb42d38
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-08-14 18:32:27 -0400

    edited: 11-second_biggest.js 8 file(s)  created:  Pushed at 2023-08-...

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 // a script that searches the second biggest integer in the list of arguments
 
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const arr = process.argv.slice(2).map(Number);
+  console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
 }
